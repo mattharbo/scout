c186a2129ddc9868a12cedad92382d34b84b529b
matthieuharbonnier <matthieu.harbonnier@coorpacademy.com>
2016-08-12 14:24:53 +0200

    project init

---
+++ index.php
@@ -0,0 +1,19 @@
+                        
+<!doctype html>
+<html lang="en">
+	<head>
+		<title>Scout • By scouters for players</title>
+		<link href='https://fonts.googleapis.com/css?family=lato:100,300,400,700' rel='stylesheet' type='text/css'>
+
+		<link rel="stylesheet" href="./css/interface.css">
+		<link rel="icon" href="./img/favicon.png">
+
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+	</head>
+	<body>
+		<div id="testdiv">
+            <a href="/" class="logo">scout</a>
+        </div>
+	</body>
+</html>
