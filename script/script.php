3d3c15669f95fed8b9189f80b89f5c65e1fccecf
mattharbo <matthieu.harbonnier@gmail.com>
2016-08-29 20:25:03 +0200

    JSON file creation

--- script/script.php
+++ script/script.php
@@ -4,45 +4,20 @@ $eventsarray=$_POST['table_content'];
 
 $gameevents = json_decode($_POST['table_content'], true);
 
-//$data = json_decode(stripslashes($_POST['data']));
-
 //$events=[[
 //   'action' => 'turnover',
-//    'team' => 'home',
-//    'time' => '32',
-//    'x' => '0.34',
-//    'y' => '0.78',   
+//   'team' => 'home',
+//   'time' => '32',
+//   'x' => '0.34',
+//   'y' => '0.78',   
 //]];
 
-//$pathtofile = "../template.json";
-//$gameevents = json_decode(file_get_contents($pathtofile), true);
+$data = json_encode($gameevents);
 
-foreach($gameevents as $record){    
-    $action = $record['action'];
-    echo $action."<br>";
-    
-    $team = $record['team'];
-    echo $team."<br>";
-    
-    $time = $record['time'];
-    echo $time."<br>";
-    
-    $xposition = $record['x'];
-    echo $xposition."<br>";
-    
-    $yposition = $record['y'];
-    echo $yposition."<br>";
-    
-    $type = $record['type'];
-    echo $type."<br>";
-    
-    $state = $record['state'];
-    echo $state."<br>";
-    
-    $judgments = $record['judgments'];
-    echo $judgments."<br>";
-}
+$newjsonfile = fopen('new_map_data.json','w+');
+fwrite($newjsonfile, $data);
+fclose($newjsonfile);
 
-
+echo "Data archived ;)";
 
 ?>
