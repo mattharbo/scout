9ac4869abb2f43183df7aac71dcc5638877b0280
mattharbo <mattharbo@users.noreply.github.com>
2016-10-19 19:06:33 +0200

    Hotfix Autocomplete

--- script/autocompletesearch.php
+++ script/autocompletesearch.php
@@ -1,21 +1,20 @@
 <?php
 
-define('DB_SERVER', 'eu-cdbr-west-01.cleardb.com');
-define('DB_USER', 'bf9408c30c58cb');
-define('DB_PASSWORD', '28dd5e4e');
-define('DB_NAME', 'heroku_2c007d1d9463440');
+//define('DB_SERVER', 'eu-cdbr-west-01.cleardb.com');
+//define('DB_USER', 'bf9408c30c58cb');
+//define('DB_PASSWORD', '28dd5e4e');
+//define('DB_NAME', 'heroku_2c007d1d9463440');
 
-//define('DB_SERVER', $GLOBALS['servername']);
-//define('DB_USER', $GLOBALS['username']);
-//define('DB_PASSWORD', $GLOBALS['password']);
-//define('DB_NAME', $GLOBALS['dbname']);
-
+define('DB_SERVER', $GLOBALS['servername']);
+define('DB_USER', $GLOBALS['username']);
+define('DB_PASSWORD', $GLOBALS['password']);
+define('DB_NAME', $GLOBALS['dbname']);
 
 if (isset($_GET['term'])){
 	$return_arr = array();
 
 	try {
-	    $conn = new PDO("mysql:host=".DB_SERVER.";port=8889;dbname=".DB_NAME, DB_USER, DB_PASSWORD);
+	    $conn = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
 	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	    
 	    $stmt = $conn->prepare('SELECT teamid, teamname, teamnickname FROM team WHERE teamname LIKE :term');
