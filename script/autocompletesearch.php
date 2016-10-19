72851a8c70f751f847c1ede1d341545207c34cce
mattharbo <mattharbo@users.noreply.github.com>
2016-10-19 19:13:40 +0200

    Hotfix Autocomplete 3

--- script/autocompletesearch.php
+++ script/autocompletesearch.php
@@ -1,9 +1,14 @@
 <?php
 
-$GLOBALS['servername']='eu-cdbr-west-01.cleardb.com';
-$GLOBALS['username']='bf9408c30c58cb';
-$GLOBALS['password']='28dd5e4e';
-$GLOBALS['dbname']='heroku_2c007d1d9463440';
+$GLOBALS['servername'] = getenv('servername');
+$GLOBALS['username'] = getenv('username');
+$GLOBALS['password'] = getenv('password');
+$GLOBALS['dbname'] = getenv('dbname');
+
+//$GLOBALS['servername']='eu-cdbr-west-01.cleardb.com';
+//$GLOBALS['username']='bf9408c30c58cb';
+//$GLOBALS['password']='28dd5e4e';
+//$GLOBALS['dbname']='heroku_2c007d1d9463440';
 
 //$GLOBALS['servername']='localhost';
 //$GLOBALS['username']='root';
