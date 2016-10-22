8296c999fee3278ff58709de3aa098ed334070b3
mattharbo <matthieu.harbonnier@gmail.com>
2016-10-22 02:00:49 +0200

    live specific view

--- v/live.php
+++ v/live.php
@@ -44,7 +44,7 @@ if (isset ($_GET["id"])) {
             $('#preloader').fadeOut('slow',function(){$(this).remove();});
         });
     </script>
-    <script type="text/javascript" src='../js/vizpage.js'></script>
+    <script type="text/javascript" src='../js/vizpagelive.js'></script>
         
 	</body>
 </html>
