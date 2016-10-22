627494d529bf0de02cc781e7aeea10bd19491d4c
mattharbo <matthieu.harbonnier@gmail.com>
2016-10-22 10:19:47 +0200

    Simple data viz (not live mode)

---
+++ js/vizpage.js
@@ -0,0 +1,24 @@
+for (var x in rawdatafromdb) {   
+    //console.log("x:"+(rawdatafromdb[x].eventposx)*100+"y:"+(rawdatafromdb[x].eventposy)*100);
+    
+    show_image('../ressources/pointer@x2.png', 
+        17, 22, 
+    (rawdatafromdb[x].eventposx)*100,
+    (rawdatafromdb[x].eventposy)*100);
+}
+
+function show_image(src, width, height, left, top) {
+    var img = document.createElement("img");
+    
+    img.src = src;
+    img.width = width;
+    img.height = height;
+    
+    // This next line will just add it to the <body> tag
+    document.getElementById("pitchContainerDataviz").appendChild(img);
+    
+    img.style.position = 'absolute';
+    img.style.left = left+'%';
+    img.style.top = top+'%';
+    
+}
